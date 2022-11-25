50d2c1c24fb804037fc77e2c20a59e6190d3949b
ilgershman <ilgershman@gmail.com>
2022-11-25 10:07:43 +0200

    activity form change

---
+++ components/ActivityForm.js
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { mutate } from "swr";
+
+const Form = ({ formId, activityForm, forNewPet = true }) => {
+  const router = useRouter();
+  const contentType = "application/json";
+  const [errors, setErrors] = useState({});
+  const [message, setMessage] = useState("");
+
+  const [form, setForm] = useState({
+    name: activityForm.name,
+    image_url: activityForm.image_url,
+  });
+
+  /* The PUT method edits an existing entry in the mongodb database. */
+  const putData = async (form) => {
+    const { id } = router.query;
+
+    try {
+      const res = await fetch(`/api/activities/${id}`, {
+        method: "PUT",
+        headers: {
+          Accept: contentType,
+          "Content-Type": contentType,
+        },
+        body: JSON.stringify(form),
+      });
+
+      // Throw error with status code in case Fetch API req failed
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      const { data } = await res.json();
+
+      mutate(`/api/activities/${id}`, data, false); // Update the local data without a revalidation
+      router.push("/activities");
+    } catch (error) {
+      setMessage("Failed to update pet");
+    }
+  };
+
+  /* The POST method adds a new entry in the mongodb database. */
+  const postData = async (form) => {
+    try {
+      const res = await fetch("/api/activities", {
+        method: "POST",
+        headers: {
+          Accept: contentType,
+          "Content-Type": contentType,
+        },
+        body: JSON.stringify(form),
+      });
+
+      // Throw error with status code in case Fetch API req failed
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      router.push("/activities");
+    } catch (error) {
+      setMessage("Failed to add pet");
+    }
+  };
+
+  const handleChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  /* Makes sure pet info is filled for pet name, owner name, species, and image url*/
+  const formValidate = () => {
+    let err = {};
+    if (!form.name) err.name = "Name is required";
+    if (!form.image_url) err.image_url = "Image URL is required";
+    return err;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errs = formValidate();
+    if (Object.keys(errs).length === 0) {
+      forNewPet ? postData(form) : putData(form);
+    } else {
+      setErrors({ errs });
+    }
+  };
+
+  return (
+    <>
+      <form id={formId} onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          maxLength="20"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="image_url">Image URL</label>
+        <textarea
+          type="url"
+          name="image_url"
+          value={form.image_url}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+      <p>{message}</p>
+      <div>
+        {Object.keys(errors).map((err, index) => (
+          <li key={index}>{err}</li>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Form;
